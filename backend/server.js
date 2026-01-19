efdcf55f582a6efc23d85763dfdf01e5aabd7e09
Sandeep-2412 <machasndp@gmail.com>
2026-01-19 10:53:49 -0500

    Changed cors

--- backend/server.js
+++ backend/server.js
@@ -15,10 +15,22 @@ console.log("ENV DEBUG:", {
 const app = express();
 const port = 4000;
 // MIDDLEWARES
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://ai-invoice-generator-six.vercel.app"
+];
+
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true); // allow server-to-server
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
+    callback(new Error("Not allowed by CORS"));
+  },
   credentials: true
 }));
+
 app.use(clerkMiddleware());
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ limit: "20mb", extended: true }));
