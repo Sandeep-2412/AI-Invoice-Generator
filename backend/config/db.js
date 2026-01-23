c1a4fd3fcb44315a97cb7018fabac6245407c0a9
Sandeep-2412 <93419414+Sandeep-2412@users.noreply.github.com>
2026-01-23 12:36:27 -0500

    Add files via upload

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 export const connectDB = async() => {
-    await mongoose.connect('your mongodb cluster uri')
+    await mongoose.connect('mongodb+srv://sandeepmacha25_db_user:idEpK0REzj8KFs2H@cluster0.e0w9dzi.mongodb.net/InvoiceAI')
     .then(() => {console.log('DB CONNECTED')});
 }
